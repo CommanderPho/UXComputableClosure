263dad3ab727a7d1af3681b6939439b786cce9b7
Pho Hale <pho@hale.io>
2016-09-16 12:40:29 -0700

    Initial Commit

---
+++ UXComputableClosure/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  UXComputableClosure
+//
+//  Created by Pho Hale on 9/16/16.
+//  Copyright © 2016 Pho Hale. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ UXComputableClosure/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UXComputableClosure
+//
+//  Created by Pho Hale on 9/16/16.
+//  Copyright © 2016 Pho Hale. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
